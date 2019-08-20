4ceb101f06fd76662d6cc133ad340131a6292530
microalgues <microalgues.ulg@gmail.com>
2019-08-20 14:03:26 +0200

    Update plot_gc_content.R

--- plot_gc_content.R
+++ plot_gc_content.R
@@ -1,5 +1,63 @@
-library(ggplot2)
+#######################################
+##           javier cordoba          ##
+##             script for            ##
+##  plotting GC content distribution ##
+#######################################
 
+###################
+## load packages ##
+###################
+library(ggplot2)
+ 
+#################################
+## load Multiple plot function ##
+#################################
+
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+#########################
+## path specifications ##
+#########################
 dir="C:/Users/LANCIOS-PC/Documents/Oficina/Labo/Paper_Transcriptome/Data/new_assembly3/"
 
 exp = c("EML","DRK","KLY","UNK","YSD","KLY.cl")
@@ -9,87 +67,34 @@ myList = list()
 dev.off()
 for (i in 1:length(exp)) {
   myList[[i]] <- read.csv(file=paste0(dir,exp[i],".txt"), sep="\t")
+  data <- myList[[i]]
+  data <- data[data$Length > 200,]
+  h = mean(data$GC)
   data <- as.data.frame(myList[[i]])
-  h = mean(myList[[i]]$GC)
-
   
-    p1 <- ggplot(data=data, aes(myList[[i]]$GC, fill= ..count..)) + 
+  p1 <- ggplot(data=data, aes(myList[[i]]$GC, fill= ..count..)) + 
     geom_histogram(colour ="black", bins = 40) + 
     geom_vline(xintercept = h, col="red") + 
     labs(y="Frequency", x= '') + 
     theme_classic() +
-    theme(legend.position = "none")
-
+    theme(legend.position = "none", text = element_text(size=20))
+  
   p2 <- ggplot(data=data, aes(myList[[i]]$GC, myList[[i]]$Length)) + 
     geom_hex(colour="black", bins = 40) + 
     labs(y='', x= '') + 
     theme_classic() + 
-    theme(legend.position = "none")
-
+    theme(legend.position = "none", text = element_text(size=20))
+  
   # export to file
   png(file= paste0("C:/Users/LANCIOS-PC/Documents/Oficina/Labo/Paper_Transcriptome/Grph/new_assembly3/pval/",
                    "GC_content_", exp[i], ".png"),
-    width=15, height=7, units="cm", res=300, pointsize=10)
+      width=15, height=7, units="cm", res=300, pointsize=10)
   
   multiplot(p1, p2, cols=2)
+  grid.text("GC content", y=1, just = "top", vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2), gp = gpar(fontsize = 20, fontface = "bold", fontfamily="Times"))
+  grid.text(paste0(exp[i]," = ",round(h, digits = 2)), y= 0.9, just = "bottom", vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2), gp = gpar(fontsize = 15, fontface = "bold", fontfamily="Times"))
+  grid.text("A", y=0.95, x= 0.08, just = "top", vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2), gp = gpar(fontsize = 15, fontface = "bold", fontfamily="Times"))
+  grid.text("B", y=0.95, x= 0.58, just = "top", vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2), gp = gpar(fontsize = 15, fontface = "bold", fontfamily="Times"))
+ 
   dev.off()
-}
-# Multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL, title="", 
-                      fontsize = 12, fontfamily = "Helvetica") {
-  require(grid)
- 
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
- 
-  numPlots = length(plots)
- 
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
- 
-  if (nchar(title)>0){
-    layout <- rbind(rep(0, ncol(layout)), layout)
-  }
- 
-  if (numPlots==1) {
-    print(plots[[1]])
- 
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), 
-                          ncol(layout), 
-                          heights = if (nchar(title)>0) {unit(c(0.5, rep(5,nrow(layout)-1)), "null")}
-                          else {unit(c(rep(5, nrow(layout))), "null")})))
- 
-    # Make each plot, in the correct location
-    if (nchar(title)>0) {
-      grid.text(title, 
-                vp = viewport(layout.pos.row = 1, layout.pos.col = 1:ncol(layout)),
-                gp = gpar(fontsize = fontsize, fontfamily = fontfamily))
-    }
- 
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
- 
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
 }
